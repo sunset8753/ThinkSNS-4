dbf86c1f76e13ad122b80757ca6cb0ed2729c52e
Seven <lovevipdsw@outlook.com>
2016-08-01 11:28:14 +0800

    #17 看下是否有解决问题呢？
    @cccRaim @lyuhaichao

--- public/pay/alipay_return.php
+++ public/pay/alipay_return.php
@@ -1,7 +1,13 @@
 <?php
 
-require_once __DIR__.'/request.php';
-$target = '/index.php?app=public&mod=Account&act=alipayReturn';
-$url = explode(basename(__FILE__), $_SERVER['REQUEST_URI']);
-$url = dirname(dirname($url[0])).$target;
-request($url);
+// require_once __DIR__.'/request.php';
+// $target = '/index.php?app=public&mod=Account&act=alipayReturn';
+// $url = explode(basename(__FILE__), $_SERVER['REQUEST_URI']);
+// $url = dirname(dirname($url[0])).$target;
+// request($url);
+
+$_GET['app'] = 'public';
+$_GET['mod'] = 'Account';
+$_GET['act'] = 'alipayReturn';
+
+require __DIR__.'/../index.php';
