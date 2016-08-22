3b4f007e5599b9745a1104162a82a51552d88ab4
Seven <lovevipdsw@outlook.com>
2016-08-22 10:39:07 +0800

    删除多余空格

--- api.php
+++ api.php
@@ -4,7 +4,7 @@ error_reporting(E_ERROR ^ E_NOTICE ^ E_WARNING);
 
 /* ///调试、找错时请去掉///前空格
 ini_set('display_errors',true);
-error_reporting(E_ALL); 
+error_reporting(E_ALL);
 set_time_limit(0);
 // */
 //网站根路径设置
