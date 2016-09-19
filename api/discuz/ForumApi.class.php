74e78ab5d067ff3dddd28c1c0465ebfdfea01ec7
Seven <lovevipdsw@outlook.com>
2016-09-19 11:08:00 +0800

    修改缩进。

--- api/discuz/ForumApi.class.php
+++ api/discuz/ForumApi.class.php
@@ -1,22 +1,31 @@
 <?php
 
-/** *  * @author jason * */require_once 'BaseApi.class.php';class ForumApi extends BaseApi
+/**
+ * 
+ * @author jason
+ *
+ */
+require_once 'BaseApi.class.php';
+class ForumApi extends BaseApi
 {
-    //获取全部版块    public function forumindex()
+    //获取全部版块
+    public function forumindex()
     {
         $content = $this->getContentFormDiscuz('forumindex');
 
         return $content;
     }
 
-    //获取热门版块    public function hotforum()
+    //获取热门版块
+    public function hotforum()
     {
         $content = $this->getContentFormDiscuz('hotforum');
 
         return $content;
     }
 
-    //获取热门帖子    public function hotthread()
+    //获取热门帖子
+    public function hotthread()
     {
         $content = $this->getContentFormDiscuz('hotthread');
 
@@ -32,7 +41,8 @@
         return $content;
     }
 
-    //获取版块帖子    public function forumdisplay()
+    //获取版块帖子
+    public function forumdisplay()
     {
         $fid = $this->data['fid'];
         $page = $this->data['page'] ? $this->data['page'] : 1;
@@ -47,7 +57,8 @@
         return $content;
     }
 
-    //获取帖子详情    public function viewthread()
+    //获取帖子详情
+    public function viewthread()
     {
         $tid = $this->data['tid'];
         $page = $this->data['page'] ? $this->data['page'] : 1;
@@ -61,23 +72,31 @@
         return $content;
     }
 
-/*"POST index.php?module=newthread&version=1&fid=40&topicsubmit=yes&mobile=no  TODO */    //发布新贴    public function addthread()
-{
-    $fid = intval($this->data['fid']);
-    $content = $this->getContentFormDiscuz('newthread', '&topicsubmit=yes&fid='.$fid, 'POST');
+/*"POST index.php?module=newthread&version=1&fid=40&topicsubmit=yes&mobile=no  TODO */
+    //发布新贴
+    public function addthread()
+    {
+        $fid = intval($this->data['fid']);
+        $content = $this->getContentFormDiscuz('newthread', '&topicsubmit=yes&fid='.$fid, 'POST');
 
-    return $content;
-}
-
-    //回复帖子    public function sendreply()
-    {        //index.php?module=sendreply&version=1&tid=4&replysubmit=yes&mobile=no
+        return $content;
     }
 
-    //收藏帖子    public function favthread()
-    {        //"POST index.php?module=favthread&version=1&id=4&favoritesubmit=true&mobile=no
+    //回复帖子
+    public function sendreply()
+    {
+        //index.php?module=sendreply&version=1&tid=4&replysubmit=yes&mobile=no
     }
 
-    //收藏版块    public function favforum()
-    {        // "GET index.php?module=favforum&version=1&id=40&mobile=no
+    //收藏帖子
+    public function favthread()
+    {
+        //"POST index.php?module=favthread&version=1&id=4&favoritesubmit=true&mobile=no
+    }
+
+    //收藏版块
+    public function favforum()
+    {
+        // "GET index.php?module=favforum&version=1&id=40&mobile=no
     }
 }
