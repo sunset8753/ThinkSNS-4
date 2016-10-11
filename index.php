a7367a53399f90acc40d5ed5de77a624418d1de2
Seven <lovevipdsw@outlook.com>
2016-10-11 14:17:18 +0800

    修复php7.1以上的兼容bug。
    这个bug很严重。

--- index.php
+++ index.php
@@ -2,10 +2,9 @@
 
 //设置错误级别
 error_reporting(E_ERROR ^ E_NOTICE ^ E_WARNING);
-// error_reporting(E_ALL);
 
 /* ///调试、找错时请去掉///前空格
-ini_set('display_errors',true);
+ini_set('display_errors', true);
 error_reporting(E_ALL);
 set_time_limit(0);
 // */
