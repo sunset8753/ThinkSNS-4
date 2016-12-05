1a8a55e71af97baff929c8e384414507b186ab31
Seven <shiweidu@outlook.com>
2016-12-05 13:08:37 +0800

    修改验证码～

--- public/captcha.php
+++ public/captcha.php
@@ -13,7 +13,8 @@ if (strtolower(ini_get('session.save_handler')) == 'files') {
 }
 session_start();
 $name = 'verify';
-$font = '../addons/library/font/ariali.ttf';
+    
+$font = dirname(dirname(__FILE__)).'/resources/assets/fonts/ariali.ttf';
 if (file_exists($font) && isset($_GET['type']) && $_GET['type'] == 'chinese') {
     Image::GBVerify(3, 'png', 140, 50, $font, $name);
 } else {
