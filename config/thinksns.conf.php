bf6252eb4ff56fbac5f468cf02f3156c9edd6fcc
Seven <shiweidu@outlook.com>
2016-12-14 21:20:16 +0800

    更新版本号～

--- config/thinksns.conf.php
+++ config/thinksns.conf.php
@@ -8,7 +8,7 @@
 return array(
 
     /* ThinkSNS v4 */
-    'VERSION' => '4.3.6', // # ThinkSNS 版本
+    'VERSION' => '4.6.0', // # ThinkSNS 版本
     'UPURL'   => 'http://demo.thinksns.com/upgrade/entry.php', // # 升级地址
 
     'exchange_type' => 5, //智播积分兑换比例 20161030 bs
